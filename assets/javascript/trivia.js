2a9987c3831d45415a130e7e957eeab5f2ebc559
B-Helgeson <brandonstevenhelgeson@gmail.com>
2018-03-13 22:08:34 -0400

    updated base code

---
+++ assets/javascript/trivia.js
@@ -0,0 +1,43 @@
+    // Questions and Data
+    var triviaQuestions = [
+        {
+            question: "Q1?",
+            answer: "A1",
+            fakes: ["A2", "A3", "A4"],
+            image: "assets/images/q1.gif",
+            gifDuration: 2240
+        },
+        {
+            question: "Q2?",
+            answer: "A1",
+            fakes: ["A2", "A3", "A4"],
+            image: "assets/images/q1.gif",
+            gifDuration: 2240
+        },
+        {
+            question: "Q3?",
+            answer: "A1",
+            fakes: ["A2", "A3", "A4"],
+            image: "assets/images/q1.gif",
+            gifDuration: 2240
+        },
+        {
+            question: "Q4?",
+            answer: "A1",
+            fakes: ["A2", "A3", "A4"],
+            image: "assets/images/q1.gif",
+            gifDuration: 2240
+        },
+        {
+            question: "Q5?",
+            answer: "A1",
+            fakes: ["A2", "A3", "A4"],
+            image: "assets/images/q1.gif",
+            gifDuration: 2240
+        }
+
+    ];
+
+
+
+console.log("connected")
